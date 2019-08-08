98858ab7fe509e19a290c96bf04c46fbf2b95e4e
Jeff Bucklew <bucklew.jeff@gmail.com>
2019-08-08 18:33:44 -0400

    change asset path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/ffb-draft-magic-vue',
+    assetsPublicPath: 'ffb-draft-magic-vue',
 
     /**
      * Source Maps
